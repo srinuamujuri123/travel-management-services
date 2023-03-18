c5f085529c1fbab1a313cc7da956e9beab5cc660
srinivas <srinuamujuri123@gmail.com>
2023-03-18 14:05:26 +0530

    updated

--- src/main/java/com/tms/model/BaseEntity.java
+++ src/main/java/com/tms/model/BaseEntity.java
@@ -3,7 +3,8 @@ package com.tms.model;
 import java.util.Date;
 
 import jakarta.persistence.MappedSuperclass;
-
+import lombok.Data;
+@Data
 @MappedSuperclass
 public class BaseEntity {
 
@@ -13,44 +14,26 @@ public class BaseEntity {
 	public Date createdOn;
 	public Date updatedOn;
 
-	public boolean isActive() {
-		return isActive;
-	}
-
-	public void setActive(boolean isActive) {
-		this.isActive = isActive;
-	}
-
-	public Integer getCreatedBy() {
-		return createdBy;
-	}
-
-	public void setCreatedBy(Integer createdBy) {
-		this.createdBy = createdBy;
-	}
-
-	public Integer getUpdatedBy() {
-		return updatedBy;
-	}
-
-	public void setUpdatedBy(Integer updatedBy) {
-		this.updatedBy = updatedBy;
-	}
-
-	public Date getCreatedOn() {
-		return createdOn;
-	}
-
-	public void setCreatedOn(Date createdOn) {
-		this.createdOn = createdOn;
-	}
-
-	public Date getUpdatedOn() {
-		return updatedOn;
-	}
-
-	public void setUpdatedOn(Date updatedOn) {
-		this.updatedOn = updatedOn;
-	}
+	/*
+	 * public boolean isActive() { return isActive; }
+	 * 
+	 * public void setActive(boolean isActive) { this.isActive = isActive; }
+	 * 
+	 * public Integer getCreatedBy() { return createdBy; }
+	 * 
+	 * public void setCreatedBy(Integer createdBy) { this.createdBy = createdBy; }
+	 * 
+	 * public Integer getUpdatedBy() { return updatedBy; }
+	 * 
+	 * public void setUpdatedBy(Integer updatedBy) { this.updatedBy = updatedBy; }
+	 * 
+	 * public Date getCreatedOn() { return createdOn; }
+	 * 
+	 * public void setCreatedOn(Date createdOn) { this.createdOn = createdOn; }
+	 * 
+	 * public Date getUpdatedOn() { return updatedOn; }
+	 * 
+	 * public void setUpdatedOn(Date updatedOn) { this.updatedOn = updatedOn; }
+	 */
 
 }
--- src/main/java/com/tms/model/CityDetails.java
+++ src/main/java/com/tms/model/CityDetails.java
@@ -5,35 +5,27 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data	//lombok used
 @Entity
 @Table(name = "CityDetails")
 public class CityDetails extends BaseEntity {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Integer cityId;
 	String cityName;
 
-	public Integer getCityId() {
-		return cityId;
-	}
-
-	public void setCityId(Integer cityId) {
-		this.cityId = cityId;
-	}
-
-	public String getCityName() {
-		return cityName;
-	}
-
-	public void setCityName(String cityName) {
-		this.cityName = cityName;
-	}
-
-	@Override
-	public String toString() {
-		return "CityDetails [cityId=" + cityId + ", cityName=" + cityName + "]";
-	}
-
+//	  public Integer getCityId() { return cityId; }
+//	  
+//	  public void setCityId(Integer cityId) { this.cityId = cityId; }
+//	  
+//	  public String getCityName() { return cityName; }
+//	  
+//	  public void setCityName(String cityName) { this.cityName = cityName; }
+//	  
+//	  @Override public String toString() { return "CityDetails [cityId=" + cityId +
+//	  ", cityName=" + cityName + "]"; }
 
 }
